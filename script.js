f7dc3e656cee4b6efeb4f90d1b9d6b50e97a324b
Fatima Sultana <fatima.shanaz30@gmail.com>
2022-05-19 19:52:08 +0530

    Selection

--- script.js
+++ script.js
@@ -32,3 +32,95 @@ const choices = {
   lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
+
+
+
+
+// Add selected styling to computer choice
+function displayComputerChoice() {
+	switch (computerChoice) {
+	  case 'rock':
+		computerRock.classList.add('selected');
+		computerChoiceEl.textContent = ' --- Rock';
+		break;
+	  case 'paper':
+		computerPaper.classList.add('selected');
+		computerChoiceEl.textContent = ' --- Paper';
+		break;
+	  case 'scissors':
+		computerScissors.classList.add('selected');
+		computerChoiceEl.textContent = ' --- Scissors';
+		break;
+	  case 'lizard':
+		computerLizard.classList.add('selected');
+		computerChoiceEl.textContent = ' --- Lizard';
+		break;
+	  case 'spock':
+		computerSpock.classList.add('selected');
+		computerChoiceEl.textContent = ' --- Spock';
+		break;
+	  default:
+		break;
+	}
+  }
+  // Check results increase scores update result texts
+  function updateScore(playerChoice){
+	if( playerChoice === computerChoice){
+	  resultText.textContent = "It's a tie!";
+	} else{
+	  const choice = choices[playerChoice];
+	  if(choice.defeats.indexOf(computerChoice) > -1){
+		startConfetti();
+		resultText.textContent = "You won!";
+		playerScoreNumber++;
+		playerScoreEl.textContent = playerScoreNumber;
+	  }
+	  else{
+		resultText.textContent = "You Lose!";
+		computerScoreNumber++;
+		computerScoreEl.textContent = computerScoreNumber;
+	  }
+	}
+  }
+  
+  // Call functions to process turn
+  function checkResults(playerChoice){
+	resetSelected();
+	computerRandomChoice();
+	displayComputerChoice();
+	updateScore(playerChoice)
+  }
+  
+  // Passsing player selection values and styling icons
+  function select(playerChoice) {
+	checkResults(playerChoice);
+	// Add 'selected' styling & playerChoice
+	switch (playerChoice) {
+	  case 'rock':
+		playerRock.classList.add('selected');
+		playerChoiceEl.textContent = ' --- Rock';
+		break;
+	  case 'paper':
+		playerPaper.classList.add('selected');
+		playerChoiceEl.textContent = ' --- Paper';
+		break;
+	  case 'scissors':
+		playerScissors.classList.add('selected');
+		playerChoiceEl.textContent = ' --- Scissors';
+		break;
+	  case 'lizard':
+		playerLizard.classList.add('selected');
+		playerChoiceEl.textContent = ' --- Lizard';
+		break;
+	  case 'spock':
+		playerSpock.classList.add('selected');
+		playerChoiceEl.textContent = ' --- Spock';
+		break;
+	  default:
+		break;
+	}
+  }
+  
+  window.select = select;
+  
+  
